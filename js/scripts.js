591549dc0054106912e054fe5580680eaa122d5b
mikmok25 <mnmmanuit.student@ua.edu.ph>
2024-05-28 18:03:59 -0400

    Added connect to wallet functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,82 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+  // Check if MetaMask is installed
+// Replace with the ABI of your token contract
+// Replace with the full ABI of your token contract
+ // Replace with the full ABI of your token contract
+      // Replace with the full ABI of your token contract
+      const tokenABI = [
+        {"inputs":[{"internalType":"uint256","name":"initialSupply","type":"uint256"}],"stateMutability":"nonpayable","type":"constructor"},
+        {"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},
+        {"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},
+        {"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},
+        {"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},
+        {"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},
+        {"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},
+        {"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"subtractedValue","type":"uint256"}],"name":"decreaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},
+        {"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"addedValue","type":"uint256"}],"name":"increaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},
+        {"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},
+        {"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},
+        {"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},
+        {"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},
+        {"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"}
+    ];
+
+    // Replace with your token contract details
+    const tokenAddress = '0x86334641030EE6a4401399560c2e5612DEee394E';
+    const tokenSymbol = 'OT'; // Replace with your token's symbol
+    const tokenDecimals = 18; // Replacej with your token's decimals
+
+    // Check if MetaMask is installed
+    if (typeof window.ethereum !== 'undefined') {
+        console.log('MetaMask is installed!');
+    } else {
+        console.log('MetaMask is not installed. Please install it to use this feature.');
+    }
+
+    async function connectWallet() {
+        try {
+            // Request account access if needed
+            const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+            // We use the first account for simplicity
+            const account = accounts[0];
+            console.log('Connected account:', account);
+
+            // Initialize web3
+            const web3 = new Web3(window.ethereum);
+
+            // Display the connected account
+            document.getElementById('wallet-address').innerText = `Connected account: ${account}`;
+
+            // Create an instance of the token contract
+            const tokenContract = new web3.eth.Contract(tokenABI, tokenAddress);
+
+            // Get the token balance of the connected account
+            const balance = await tokenContract.methods.balanceOf(account).call();
+            const formattedBalance = web3.utils.fromWei(balance, 'ether');
+            document.getElementById('token-balance').innerText = `Token balance: ${formattedBalance}`;
+
+            // Add the token to MetaMask
+            const wasAdded = await ethereum.request({
+                method: 'wallet_watchAsset',
+                params: {
+                    type: 'ERC20', // Token standard
+                    options: {
+                        address: tokenAddress, // The address that the token is at.
+                        symbol: tokenSymbol, // A ticker symbol or shorthand, up to 5 chars.
+                        decimals: tokenDecimals, // The number of decimals in the token
+                    },
+                },
+            });
+
+            if (wasAdded) {
+                console.log('Token added to MetaMask');
+            } else {
+                console.log('Token not added to MetaMask');
+            }
+        } catch (error) {
+            console.error('Error connecting to MetaMask:', error);
+        }
+    }
